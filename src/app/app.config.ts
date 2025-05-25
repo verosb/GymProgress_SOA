5e7cca83472f90d52103d604a25f1c4f0c9eb5e2
IbethOrtegaa <idortegar@ufpso.edu.co>
2025-05-25 16:07:13 -0500

    Configuración de Typescript

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core'; 
 import { provideRouter } from '@angular/router';
-
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser'; //habilita la hidrtacion del cliente en aplicaciones SSR para mejorar velocidad
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { initializeApp } from 'firebase/app'; //inicializacion de firebase 
+
+initializeApp(environment.firebase);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      HttpClientModule,
+      AngularFireModule.initializeApp(environment.firebase),
+      AngularFirestore
+    ),
+  ],
 };
-
-//Configuracion global de angular 
-//Configuración de lo que en versiones anteriores era app.module.ts
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
-//Define las rutas de navegación dentro de la aplicación
+import { authGuard } from './guards/auth.guard';
+
 export const routes: Routes = [
   {
-    path: "auth",
+    path: 'auth',
     loadComponent: () => import('./auth/auth.component'),
     children: [
       {
@@ -15,19 +16,55 @@ export const routes: Routes = [
         title: 'Register',
         loadComponent: () => import('./auth/register/register.component'),
       },
+      {
+        path: 'forgot-password',
+        title: 'ForgotPassword',
+        loadComponent: () => import('./auth/forgot-password/forgot-password.component'),
+      },
+      {
+        path: 'reset-password',
+        title: 'Reset-password',
+        loadComponent: () => import('./auth/reset-password/reset-password.component'),
+      }
     ],
   },
   {
-    path: "dashboard",
+    path: 'dashboard',
+    canActivate: [authGuard],
     loadComponent: () => import('./dashboard/dashboard.component'),
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        title: 'Home',
+        loadComponent: () => import('./pages/home/home.component'),
+      },
+      {
+        path: 'routines',
+        title: 'Routines',
+        loadComponent: () => import('./pages/routines/routines.component'),
+      },
+    ],
   },
   {
-    //Envio de una vez al login apenas cargue el programa 
     path: '',
     redirectTo: 'auth/login',
     pathMatch: 'full',
-  },
+  }
+  ,
+  {
+    path: 'github-callback',
+    loadComponent: () =>
+      import('./auth/github-callback/github-callback.component').then(m => m.GithubCallbackComponent)
+  }
+  ,
+  {
+    path: 'facebook-callback',
+    loadComponent: () =>
+      import('./auth/facebook-callback/facebook-callback.component').then(m => m.FacebookCallbackComponent)
+  }
 ];
-
-
-
