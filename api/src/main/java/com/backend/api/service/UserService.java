2d917d12b17ee8893945d607d23671c83ceb9a21
IbethOrtegaa <idortegar@ufpso.edu.co>
2025-05-25 15:49:52 -0500

    Configuración de Rutas

--- api/src/main/java/com/backend/api/service/UserService.java
+++ api/src/main/java/com/backend/api/service/UserService.java
@@ -1,19 +1,26 @@
 package com.backend.api.service;
 
+import com.backend.api.model.PasswordResetToken;
 import com.backend.api.model.User;
 import com.backend.api.repository.UserRepository;
+import com.backend.api.repository.PasswordResetTokenRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordResetTokenRepository passwordResetTokenRepository;
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
@@ -30,5 +37,17 @@ public class UserService {
     public Optional<User> getUserByEmail(String email) {
         return userRepository.findByEmail(email);
     }
+
+    public String createPasswordResetToken(User user) {
+    String token = UUID.randomUUID().toString();
+    PasswordResetToken resetToken = new PasswordResetToken();
+    resetToken.setToken(token);
+    resetToken.setUser(user);
+    resetToken.setExpirationDate(LocalDateTime.now().plusHours(1)); // válido por 1 hora
+
+    passwordResetTokenRepository.save(resetToken);
+
+    return token;
+}
 
 }
